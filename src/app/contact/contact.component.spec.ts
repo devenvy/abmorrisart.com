ab9f2c6329eec5d33a0ddba2fff0fa7cd58e5866
Ryan Morris <ryan@devenvy.com>
2021-06-07 18:13:14 -0500

    contact tests

---
+++ src/app/contact/contact.component.spec.ts
@@ -0,0 +1,41 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ContactComponent } from './contact.component';
+
+describe('ContactComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        ContactComponent,
+      ],
+    }).compileComponents();
+  });
+
+  it('should create component instance', () => {
+    const fixture = TestBed.createComponent(ContactComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should render title', async () => {
+    const fixture = TestBed.createComponent(ContactComponent);
+    await fixture.whenStable();
+
+    const titleEl = fixture.nativeElement.querySelector('.hero-title');
+
+    expect(titleEl?.textContent?.trim()).toEqual('Don’t be shy.  Say Hello.');
+  });
+
+  it('should render working links', async () => {
+    const fixture = TestBed.createComponent(ContactComponent);
+    await fixture.whenStable();
+
+    const socialLinkEls = fixture.nativeElement.querySelectorAll('.social-list a');
+
+    expect(socialLinkEls.length).toEqual(10);
+    socialLinkEls.forEach((x: HTMLAnchorElement) => expect(x.href).toBeTruthy());
+  });
+});
